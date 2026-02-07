b24d69bfb620cf92902f0348e15d9a8df1546afc
Nasreen2115 <nasreen2115shaik@gmail.com>
2026-02-07 14:40:30 +0530

    Add files via upload

---
+++ Operatorsex.java
@@ -0,0 +1,19 @@
+public class Operatorsex{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int sum=a+b; //sum of two numbers
+        System.out.println("sum of two numbers:"+sum);
+        if(a>=0){
+            System.out.println("The given number is positive"); 
+        }
+        if (a>0 && a/2==0) {
+            System.out.println("The number is positive and even"); 
+
+        }
+        int c=100; 
+        System.out.println("The value of c :"+c);
+        System.out.println(++a);
+
+    }
+}
