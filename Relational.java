6fd6d2897241a3b9b80d6ade5bef937fc0f8b18a
Nasreen2115 <nasreen2115shaik@gmail.com>
2026-02-07 14:20:00 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,13 @@
+public class Relational{
+    public static void main(String args[]) 
+    {
+        float a=1.23f;
+        float b=5.6f;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b); 
+    }
+}
