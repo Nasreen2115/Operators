e5219351a848a40b23f606768708a675ef1d19ef
Nasreen2115 <nasreen2115shaik@gmail.com>
2026-02-07 15:09:49 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,15 @@
+public class Swapping{
+    public static void main(String args[]){
+       // Scanner sc=new Scanner(System.in);
+        int a=100;
+        int b=20;
+        int temp;
+        System.out.println("before swapping"+a);
+        System.out.println("before swapping"+b);
+        temp=a;
+        a=b;
+        b=temp; 
+        System.out.println("after swapping"+a );
+        System.out.println("before swapping"+b);
+    }
+}
